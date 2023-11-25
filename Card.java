1f4e649ad4d708d5da092ff96d0646956a62f0d1
hyrinn <126777014+hyrinn@users.noreply.github.com>
2023-11-25 23:36:43 +0900

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Card extends JFrame{
+    JButton b1, b2, b3;
+    Container cPane;
+    CardLayout layoutm;
+    public Card(){
+        setTitle("BorderLayout Test");
+        setSize(400,200);
+        cPane = getContentPane();
+        layoutm = new CardLayout();
+        setLayout(layoutm);
+
+        JButton b1 = new JButton("Card #1");
+        JButton b2 = new JButton("Card #2");
+        JButton b3 = new JButton("Card #3");
+
+        add(b1);
+        add(b2);
+        add(b3);
+        b1.addActionListener(e->layoutm.next(cPane));
+        b2.addActionListener(e->layoutm.previous(cPane));
+        b3.addActionListener(e->layoutm.next(cPane));
+
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public static void main(String args[]){
+        Card c = new Card();
+    }
+}
